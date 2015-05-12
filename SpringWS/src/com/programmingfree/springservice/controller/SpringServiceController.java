90d51e1489f8716b817f6773b1fc3e9ae224ffa3
ramakrishnareddy_b <ramakrishnareddy_b@SS-LAP-220.semanticindia.com>
2015-05-12 15:45:07 +0530

    123

---
+++ SpringWS/src/com/programmingfree/springservice/controller/SpringServiceController.java
@@ -0,0 +1,17 @@
+package com.programmingfree.springservice.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/greeting")
+public class SpringServiceController {
+	@RequestMapping(value = "/{userName}/{password}", method = RequestMethod.GET)
+	public String getGreeting(@PathVariable String userName, @PathVariable String password) {
+		String result="Hello "+userName+":::"+password;		
+		return result;
+	}
+}
